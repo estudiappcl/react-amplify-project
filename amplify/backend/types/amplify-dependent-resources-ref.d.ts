60373cafaf1d6ee078cb2b06ea6f4af2f8112f15
Estudiappcl <estudiapp2022@gmail.com>
2024-02-12 23:39:08 -0300

    added notas

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactamplifyproject": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "reactamplifyproject82b6dea3": {
       "AppClientID": "string",
